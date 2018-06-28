e4f31debe2963bcb52a86577b2358df127373444
Alan Davila <alandavila@Alans-MacBook-Pro.local>
2018-06-28 14:56:29 -0500

    new mongoose code

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const mongoose = require("mongoose");
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const blogPosts = [];
+const Blog = require("./models/blog");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+mongoose.connect("mongodb://localhost/my-blog");
 
 // This allows us to serve files out of the client/build folder
 app.use(express.static("client/build"));
@@ -25,8 +26,9 @@ app.get("/api/test", (req, res) => {
 
 app.post("/api/blog", (req, res) => {
     console.log(req.body);
-    blogPosts.push(req.body);
-    res.json(blogPosts);
+    Blog.create(req.body).then(dbBlog => {
+        res.json(dbBlog); 
+    })
 })
 
 // This is a catch all if no other routes are matched
